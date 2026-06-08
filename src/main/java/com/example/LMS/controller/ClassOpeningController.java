5ac38d19129f48be0971d94dc8f526eb3fc30548
QuocKiet020105 <kullkiet725@gmail.com>
2026-06-08 11:17:41 +0700

    fix danh sach mon va lop hoc

--- src/main/java/com/example/LMS/controller/ClassOpeningController.java
+++ src/main/java/com/example/LMS/controller/ClassOpeningController.java
@@ -214,18 +214,11 @@ public class ClassOpeningController {
         // Thêm endpoint phần xem danh sách môn học và lớp học cho Sinh viên
         @GetMapping("/courses-with-classes")
         @PreAuthorize("hasAuthority('COURSE_CLASS_VIEW')")
-        @Operation(summary = "Danh sách môn học kèm lớp học phần",
-                description = "Dành cho sinh viên xem để đăng ký học phần")
-        public ApiResponse<Page<CourseWithClassesResponse>> getCoursesWithClasses (
-                @RequestParam(required = false) String keyword,
-                @RequestParam(required = false) Long departmentId,
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "10") int size
-    ){
-            return ApiResponse.<Page<CourseWithClassesResponse>>builder()
+        public ApiResponse<List<CourseWithClassesResponse>> getCoursesWithClasses() {
+            return ApiResponse.<List<CourseWithClassesResponse>>builder()
                     .code(200)
                     .message("Tải danh sách môn học và lớp học thành công!")
-                    .data(classOpeningService.getCoursesWithClasses(keyword, departmentId, page, size))
+                    .data(classOpeningService.getCoursesWithClasses())
                     .build();
         }
     }
