5937e323ccfadcf526d6db7b1cab91903750f50a
Ntt1110 <trungntt999@gmail.com>
2026-06-13 09:30:04 +0700

    fix strong password + sinh token cho restpass

--- src/main/java/com/example/LMS/dto/request/ChangePasswordDto.java
+++ src/main/java/com/example/LMS/dto/request/ChangePasswordDto.java
@@ -1,6 +1,7 @@
 package com.example.LMS.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -12,5 +13,9 @@ public class ChangePasswordDto {
 
     @NotBlank(message = "Mật khẩu mới không được để trống")
     @Size(min = 6, message = "Mật khẩu mới phải chứa ít nhất 6 ký tự")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password phải bao gồm ít nhất 1 chữ hoa, 1 chữ thường, 1 chữ số và 1 ký tự đặc biệt (@$!%*?&)"
+    )
     private String newPassword;
 }
